6e12591619fe6ba7fb18cbd5cc1abf9ec756587c
sol <msfontenla@fi.uba.ar>
2021-06-01 13:32:57 -0300

    fixes en el preprocessing

--- parte_2/preprocessing.py
+++ parte_2/preprocessing.py
@@ -2,8 +2,8 @@ import requests
 import numpy as np
 import pandas as pd
 
-DATA_VALIDATION = "impuestos_train.csv"
-DATA_TRAIN = "impuestos_val.csv"
+DATA_VALIDATION = "impuestos_val.csv"
+DATA_TRAIN = "impuestos_train.csv"
 DATA_VALIDATION_URL = "https://docs.google.com/spreadsheets/d/1ObsojtXfzvwicsFieGINPx500oGbUoaVTERTc69pzxE/export?format=csv"
 DATA_TRAIN_URL = "https://docs.google.com/spreadsheets/d/1-DWTP8uwVS-dZY402-dm0F9ICw_6PNqDGLmH0u8Eqa0/export?format=csv"
 
@@ -31,13 +31,13 @@ def prepararSetDeEntrenamiento(train_df:pd.DataFrame):
     #no se si estaria bien borrar las filas que borrabamos en el primer tp
     train_df.drop(columns=['religion','horas_trabajo_registradas'], inplace=True)
     train_df.fillna(np.nan, inplace = True)
-    
+
     train_df['categoria_de_trabajo'] = train_df['categoria_de_trabajo'].replace(np.nan, 'No respondio')
     train_df['trabajo'] = train_df['trabajo'].replace(np.nan, 'No respondio')
     train_df['barrio'] = train_df['barrio'].replace(np.nan, 'Otro Barrio')
-    
+
     #train_df = conversionDeVariablesCategoricas(train_df)
-    
+
     return train_df
 
 def prepararSetDeValidacion(validation_df:pd.DataFrame):
